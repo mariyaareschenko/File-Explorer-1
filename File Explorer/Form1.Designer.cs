60cc6ac2fa9ee0bd1788d6f0f0b3b82a7a0a1827
мария арещенко <mariya.areschenko@gmail.com>
2022-04-28 23:35:50 +0300

    Добавьте файлы проекта.

---
+++ File Explorer/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿
+namespace File_Explorer
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.treeView = new System.Windows.Forms.TreeView();
+            this.imageList = new System.Windows.Forms.ImageList(this.components);
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.editingToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.copyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pasteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listView = new System.Windows.Forms.ListView();
+            this.ColumnHeaderName = new System.Windows.Forms.ColumnHeader();
+            this.Type = new System.Windows.Forms.ColumnHeader();
+            this.LastMod = new System.Windows.Forms.ColumnHeader();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.menuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Cursor = System.Windows.Forms.Cursors.VSplit;
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.treeView);
+            this.splitContainer1.Panel1.Controls.Add(this.menuStrip);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.listView);
+            this.splitContainer1.Size = new System.Drawing.Size(800, 450);
+            this.splitContainer1.SplitterDistance = 266;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // treeView
+            // 
+            this.treeView.Cursor = System.Windows.Forms.Cursors.Default;
+            this.treeView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView.ImageIndex = 0;
+            this.treeView.ImageList = this.imageList;
+            this.treeView.Location = new System.Drawing.Point(0, 28);
+            this.treeView.Name = "treeView";
+            this.treeView.SelectedImageIndex = 0;
+            this.treeView.Size = new System.Drawing.Size(266, 422);
+            this.treeView.TabIndex = 0;
+            this.treeView.BeforeExpand += new System.Windows.Forms.TreeViewCancelEventHandler(this.treeView_BeforeExpand);
+            this.treeView.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView_AfterSelect);
+            // 
+            // imageList
+            // 
+            this.imageList.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList.ImageStream")));
+            this.imageList.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList.Images.SetKeyName(0, "folder.png");
+            this.imageList.Images.SetKeyName(1, "text.png");
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.editingToolStripMenuItem});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(266, 28);
+            this.menuStrip.TabIndex = 1;
+            this.menuStrip.Text = "menuStrip1";
+            // 
+            // editingToolStripMenuItem
+            // 
+            this.editingToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.copyToolStripMenuItem,
+            this.pasteToolStripMenuItem});
+            this.editingToolStripMenuItem.Name = "editingToolStripMenuItem";
+            this.editingToolStripMenuItem.Size = new System.Drawing.Size(74, 24);
+            this.editingToolStripMenuItem.Text = "Правка";
+            // 
+            // copyToolStripMenuItem
+            // 
+            this.copyToolStripMenuItem.Name = "copyToolStripMenuItem";
+            this.copyToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.copyToolStripMenuItem.Text = "Копировать";
+            this.copyToolStripMenuItem.Click += new System.EventHandler(this.copyToolStripMenuItem_Click);
+            // 
+            // pasteToolStripMenuItem
+            // 
+            this.pasteToolStripMenuItem.Name = "pasteToolStripMenuItem";
+            this.pasteToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.pasteToolStripMenuItem.Text = "Вставить";
+            this.pasteToolStripMenuItem.Click += new System.EventHandler(this.pasteToolStripMenuItem_Click);
+            // 
+            // listView
+            // 
+            this.listView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.ColumnHeaderName,
+            this.Type,
+            this.LastMod});
+            this.listView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView.HideSelection = false;
+            this.listView.Location = new System.Drawing.Point(0, 0);
+            this.listView.Name = "listView";
+            this.listView.Size = new System.Drawing.Size(530, 450);
+            this.listView.SmallImageList = this.imageList;
+            this.listView.TabIndex = 0;
+            this.listView.UseCompatibleStateImageBehavior = false;
+            this.listView.View = System.Windows.Forms.View.Details;
+            // 
+            // ColumnHeaderName
+            // 
+            this.ColumnHeaderName.Text = "Имя";
+            // 
+            // Type
+            // 
+            this.Type.Text = "Тип";
+            // 
+            // LastMod
+            // 
+            this.LastMod.Text = "Последнее изменение";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.splitContainer1);
+            this.MainMenuStrip = this.menuStrip;
+            this.Name = "Form1";
+            this.Text = "Проводник";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TreeView treeView;
+        private System.Windows.Forms.ListView listView;
+        private System.Windows.Forms.ImageList imageList;
+        private System.Windows.Forms.ColumnHeader ColumnHeaderName;
+        private System.Windows.Forms.ColumnHeader Type;
+        private System.Windows.Forms.ColumnHeader LastMod;
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem editingToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem copyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pasteToolStripMenuItem;
+    }
+}
+
---
+++ File Explorer/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace File_Explorer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            var root = new TreeNode() { Text = "C:", Tag = "c:\\" };
+            treeView.Nodes.Add(root);
+            Build(root);
+            root.Expand();
+
+        }
+        private void Build(TreeNode parent)
+        {
+            var path = parent.Tag as string;
+            parent.Nodes.Clear();
+            try
+            {
+                foreach(var dir in Directory.GetDirectories(path))
+                {
+                    parent.Nodes.Add(new TreeNode(Path.GetFileName(dir), new[] { new TreeNode("...") }) { Tag = dir });
+                }
+                foreach(var file in Directory.GetFiles(path))
+                {
+                    parent.Nodes.Add(new TreeNode(Path.GetFileName(file), 1, 1) { Tag = file });
+                }
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void AddAllFolders(TreeNode tNode, string folderPath)
+        {
+            try
+            {
+                foreach (string folderNode in Directory.GetDirectories(folderPath))
+                {
+                    TreeNode subFolder = tNode.Nodes.Add(folderNode.Substring(folderNode.LastIndexOf(@"\") + 1));
+                    subFolder.Tag = folderNode;
+                    subFolder.Nodes.Add("Загрузка...");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка");
+            }
+        }
+
+        private void treeView_BeforeExpand(object sender, TreeViewCancelEventArgs e)
+        {
+            Build(e.Node);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            treeView.Sort();
+            TreeNode treeNode = treeView.Nodes.Add("(Drive C:)");
+            AddAllFolders(treeNode, @"C:\");
+        }
+
+        private void treeView_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            string fileExtension = null;
+            string dateMod = null;
+            string folder = Convert.ToString(treeView.SelectedNode.Tag);
+            int itemIndex = 0;
+            listView.Items.Clear();
+            if (treeView.SelectedNode.Nodes.Count == 1 && treeView.SelectedNode.Nodes[0].Text == "Загрузка...")
+            {
+                treeView.SelectedNode.Nodes.Clear();
+                AddAllFolders(treeView.SelectedNode, Convert.ToString(treeView.SelectedNode.Tag));
+            }
+            if ((folder != null) && System.IO.Directory.Exists(folder))
+            {
+                try
+                {
+                    foreach (string file in System.IO.Directory.GetFiles(folder))
+                    {
+                        fileExtension = System.IO.Path.GetExtension(file);
+                        dateMod = System.IO.File.GetLastWriteTime(file).ToString();
+                        listView.Items.Add(file.Substring(file.LastIndexOf(@"\") + 1), file.ToString());
+                        listView.Items[itemIndex].SubItems.Add(fileExtension.ToString() + " Файл");
+                        listView.Items[itemIndex].SubItems.Add(dateMod.ToString());
+                        itemIndex += 1;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Ошибка");
+                }
+            }
+        }
+
+        private void copyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ListView.SelectedListViewItemCollection lvSel = this.listView.SelectedItems;
+            string strFileName = null;
+            foreach (ListViewItem listItem in lvSel)
+            {
+                strFileName = treeView.SelectedNode.Tag + @"\" + listItem.Text;
+                DataObject dataObject = new DataObject();
+                string[] cbBoardFile = new string[1];
+                cbBoardFile[0] = strFileName;
+                dataObject.SetData(DataFormats.FileDrop, true, cbBoardFile);
+                Clipboard.SetDataObject(dataObject);
+                MessageBox.Show("Файл скопирован в буфер обмена");
+            }
+        }
+
+        private void pasteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            IDataObject dataObject = Clipboard.GetDataObject();
+            if (dataObject.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] strClipFile = (string[])dataObject.GetData(DataFormats.FileDrop);
+                int i = 0;
+                for (i = 0; i <= strClipFile.Length - 1; i++)
+                {
+                    if (File.Exists(treeView.SelectedNode.Tag + "/" + Path.GetFileName(strClipFile[i])))
+                    {
+                        File.Move(treeView.SelectedNode.Tag + "/" + Path.GetFileName(strClipFile[i]), treeView.SelectedNode.Tag + "temp");
+
+                    }
+                    File.Copy(strClipFile[i], treeView.SelectedNode.Tag + "/" + Path.GetFileName(strClipFile[i]));
+                }
+                MessageBox.Show("Файл вставлен из буфера обмена");
+            }
+        }
+    }
+}
